4c5ffe2bb4d963b2730533fb9bfa68b0c0c76524
Zinc <qianyuxin2003@gamil.com>
2025-04-28 18:03:59 +0800

    build(windows): 添加 video_player_win 插件支持
    - 在 pubspec.yaml 中添加 video_player_win 依赖
    - 更新 generated_plugin_registrant.cc 和 generated_plugins.cmake 以支持新插件

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -8,10 +8,13 @@
 
 #include <audioplayers_windows/audioplayers_windows_plugin.h>
 #include <desktop_drop/desktop_drop_plugin.h>
+#include <video_player_win/video_player_win_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AudioplayersWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("AudioplayersWindowsPlugin"));
   DesktopDropPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("DesktopDropPlugin"));
+  VideoPlayerWinPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("VideoPlayerWinPluginCApi"));
 }
